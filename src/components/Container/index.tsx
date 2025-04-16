69ea4ab951eb61e2c78cd13a8b80b41fbb7d7fe8
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-04-16 14:00:03 -0300

    update

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -6,7 +6,7 @@ interface propConatiner {
 
 export function Container({ children }: propConatiner) {
   return (
-    <section className="flex flex-col items-center border-2 w-full h-full px-5 md:px-20">
+    <section className="flex flex-col items-center w-full h-full px-5 md:px-20">
       {children}
     </section>
   );
